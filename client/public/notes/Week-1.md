3b63248fbb698fb9795aac797b3f5ed3ebbd0904
Ben <bcberedo17@gmail.com>
2025-09-24 10:23:30 -0400

    Slight update to Week 1 notes and resources page

--- client/public/notes/Week-1.md
+++ client/public/notes/Week-1.md
@@ -1,21 +1,21 @@
 # Week 1: Shell Basics
-A _____ is the main program of an operating system that loads tasks and scripts into _____.
+A **_____** is the main program of an operating system that loads tasks and scripts into **_____**.
 
-A ______ lets us interact with the _______.
+A **______** lets us interact with the **_______**.
 
-Most computers have apps called _______ that can access this s___l.
-CLI, which stands for ____ _____ ________, is sometimes used to describe terminals, especially when they are being compared to the GUI, which stands for ______ ______ ______.
+Most computers have apps called _______ that can access this **s___l**.
+CLI, which stands for **____ _____ ________**, is sometimes used to describe terminals, especially when they are being compared to the GUI, which stands for **______ ______ ______**.
 
 
 ### Common terminal commands:
-**pwd** - prints the "working directory", aka the folder that we are working with in the terminal
-**ls**  - ???
+**pwd** - prints the "working directory", aka the folder that we are working with in the terminal  
+**ls**  - ???  
 **cd**  - ???
 
 Note: you can typically put `-h` or `--h___` after a command to find out what it does and what parameters it takes. These hyphenated inputs are called *flags*.
 
 ### Netcat / ncat
-This command line utility lets you connect to a open ____ on a computer on a different _______. The basic syntax for this tool is `nc <IP / domain name> <port>`.
+This command line utility lets you connect to a open **____** on a computer on a different **_______**. The basic syntax for this tool is `nc <IP / domain name> <port>`.
 
 Use `nc --help` or `man nc` for more info.
 
--- client/src/pages/Resources.tsx
+++ client/src/pages/Resources.tsx
@@ -10,17 +10,17 @@ export default function Resources() {
 
         <h2>The GitHub Codespace</h2>
         <p>
-          Those who can't download VSCode or access the terminal on their personal device should create a GitHub codespace with <TooltipLink href="https://github.com/codespaces/new?repo=scepterb/hack-cats-codespace" tooltip="github.com">this link</TooltipLink>.
+          Those who can't download VSCode or access the terminal on their personal device should create a GitHub codespace with <TooltipLink href="https://github.com/codespaces/new?repo=scepterb/hack-cats-codespace" tooltip="github.com" target="_blank">this link</TooltipLink>.
           To do so, you must create a <Annotation note="Github is a website allowing programmers to store their projects in things called 'repositories' so that others can collaborate">GitHub account.</Annotation> 
-          GitHub codespaces are basically a cloud computer running VSCode for you. We'll use it to write code and connect to remote computers.
+          <em>GitHub codespaces</em> are basically a cloud computer running VSCode for you. We'll use it to write code and connect to remote computers.
         </p>
         <p>
           This particular codespace will automatically install certain tools for you, including 
           <ul>
             <li>nmap,</li>
-            <li>the Python language + <TooltipLink href="https://pypi.org/project/pip/" tooltip="pip on pypi.org">pip</TooltipLink>,</li>
-            <li><TooltipLink href="https://hashcat.net/wiki/doku.php?id=hashcat" tooltip="hashcat.net">hashcat</TooltipLink>,</li>
-            <li><TooltipLink href="https://man.openbsd.org/nc.1" tooltip="man.openbsd.org">netcat-openbsd</TooltipLink>,</li>
+            <li>the Python language + <TooltipLink href="https://pypi.org/project/pip/" tooltip="pip on pypi.org" target="_blank">pip</TooltipLink>,</li>
+            <li><TooltipLink href="https://hashcat.net/wiki/doku.php?id=hashcat" tooltip="hashcat.net" target="_blank">hashcat</TooltipLink>,</li>
+            <li><TooltipLink href="https://man.openbsd.org/nc.1" tooltip="man.openbsd.org" target="_blank">netcat-openbsd</TooltipLink>,</li>
             <li>smbclient,</li>
             <li>ncftp,</li>
             <li>gobuster,</li>
