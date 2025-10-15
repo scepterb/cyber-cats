a704b9d38a6876afba81d2ca5fdd887f20c9df77
Ben <bcberedo17@gmail.com>
2025-10-14 22:34:20 -0400

    Start Week 2 Stuff

--- client/src/pages/Notes.tsx
+++ client/src/pages/Notes.tsx
@@ -29,9 +29,19 @@ export default function Notes() {
         homework={<TooltipLink tooltip='play.picoctf.org' href='https://play.picoctf.org/playlists/18?m=154'>"Tab, Tab, Attack"</TooltipLink>}
         color="#b18acd5f"
       >
-        In today's session, we learned what operating systems are and the importance of understanding Linux basics. <br/>
+        This week, we learned what operating systems are and the importance of understanding Linux basics.
         We also reviewed other resources for learning and reviewing hacking skills (see the <Link to="/resources">/resources page</Link>).
       </WeekNote>
+      <WeekNote
+        week='2'
+        slidesUrl=''
+        notesUrl='./notes/Week-2.md'
+        challenge={<TooltipLink tooltip='play.picoctf.org' href='https://play.picoctf.org/practice/challenge/414?category=5&page=1'>"Endianness"</TooltipLink>}
+        homework={<TooltipLink tooltip='pwn.college' href='https://pwn.college/linux-luminarium/hello/'>Pwn.college "Hello, Hackers"</TooltipLink>}
+        color="#cf72b65f">
+          This week, we learned binary and hex represenation. We learned about bits and bytes, endianness, and encodings such as ASCII.<br/>
+          The challenge is short this week, so why not try out that homework? It's good review.
+        </WeekNote>
     </div>
   );
 }
